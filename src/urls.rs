ad5048d6ab55e433a4f504015e53058961a8c108
M0dEx <jakub.kubik.it@protonmail.com>
2022-01-31 14:37:09 +0100

    feat: add additional URL templates

--- src/urls.rs
+++ src/urls.rs
@@ -1,3 +1,8 @@
 pub const LOGIN_URL: &str = "/auth/login.json";
 pub const VERIFY_URL: &str = "/auth/verify.json";
 pub const ME_URL: &str = "/users/me.json";
+
+pub const RESOURCES_URL: &str = "/resources.json";
+pub const RESOURCE_URL: &str = "/resources/{}.json";
+
+pub const SECRET_URL: &str = "/secrets/resource/{}.json";
